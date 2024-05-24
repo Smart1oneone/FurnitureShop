406895631173ac53bb942cc656aa74a0d2723078
smartone <smart1oneone@gmail.com>
2024-05-24 15:08:23 +0300

    Added pagination

--- goods/views.py
+++ goods/views.py
@@ -1,18 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from goods.models import Products
-def catalog(request):
-    goods = Products.objects.all()
+from django.core.paginator import Paginator
+def catalog(request, category_slug, page=1):
+    if category_slug == 'vse-tovary':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3, )
+    current_page = paginator.page(page)
+
+
     context = {
         'title' : 'Home - Каталог',
-        'goods': goods,
+        'goods': current_page,
+        'category_slug' : category_slug,
+        'slug_url': category_slug,
     }
 
 
     return render(request, 'goods/catalog.html', context)
 
-def product(request):
-    context = [
+def product(request, product_slug):
 
-    ]
+    product = Products.objects.get(slug=product_slug)
+    context = {
+        'product': product,
+    }
 
     return render(request, 'goods/product.html', context)

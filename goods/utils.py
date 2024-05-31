027abe5a86cbc51c7ffdb5f723f866c21a45bb55
smartone <smart1oneone@gmail.com>
2024-05-31 12:18:53 +0300

    Full text search finished

--- goods/utils.py
+++ goods/utils.py
@@ -1,6 +1,6 @@
 from django.db.models import Q
 from goods.models import Products
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector, SearchHeadline
 
 
 def q_search(query):
@@ -11,11 +11,10 @@ def q_search(query):
 
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
-    # keywords = [word for word in query.split() if len(word) > 2]
-    #
-    # q_objects = Q()
-    #
-    # for token in keywords:
-    #     q_objects |= Q(description__icontains=token)
-    # return Products.objects.filter(q_objects)
+    result = Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
+
+    result = result.annotate(headline=SearchHeadline('name', query, start_sel = '<span style="background-color:yellow;">', stop_sel = "</span>"),)
+    result = result.annotate(bodyline=SearchHeadline('name', query, start_sel = '<span style="background-color:yellow;">', stop_sel = "</span>"),)
+
+
+    return result

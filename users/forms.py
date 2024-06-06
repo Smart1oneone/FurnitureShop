a27f73e66c52e7bd4b7d8a1327b789bb536c789c
smartone <smart1oneone@gmail.com>
2024-06-06 12:32:27 +0300

    added registration form

--- users/forms.py
+++ users/forms.py
@@ -1,21 +1,35 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from users.models import User
-class UserLoginForm(forms.ModelForm):
-    username = forms.CharField(label='Имя пользователя',
-        widget=forms.TextInput(attrs={"autofocus": True,
-
-        "class": "form-control",
-        "placeholder": "Введите ваше имя пользвателя",
-        }))
-    password = forms.CharField(
-        label= "Пароль",
-        strip=False,
-        widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
-                                                          "class": "form-control",
-    "placeholder": "Введите ваш пароль",
-    }),
-    )
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField()
+    password = forms.CharField()
+        # username = forms.CharField(label='Имя пользователя',
+        #     widget=forms.TextInput(attrs={"autofocus": True,
+        #
+        #     "class": "form-control",
+        #     "placeholder": "Введите ваше имя пользвателя",
+        #     }))
+        # password = forms.CharField(
+        #     label= "Пароль",
+        #     strip=False,
+        #     widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+        #                                                       "class": "form-control",
+        # "placeholder": "Введите ваш пароль",
+        # }),
+        # )
     class Meta:
         model = User
         fields = ['username', 'password']
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('first_name',
+                  'last_name',
+                  'username',
+                  'email',
+                  'password1',
+                  'password2',
+                  )
+
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from users.forms import UserLoginForm
+from django.shortcuts import render, redirect
+from users.forms import UserLoginForm, UserRegistrationForm
 from django.contrib import auth
 from django.urls import reverse
 from django.http import HttpResponseRedirect
@@ -23,8 +23,18 @@ def login(request):
     }
     return render(request, 'users/login.html', context)
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
     context = {
-        'title' : 'Регистрация'
+        'title' : 'Регистрация',
+        'form': form,
     }
     return render(request, 'users/registration.html', context)
 def profile(request):
@@ -33,4 +43,5 @@ def profile(request):
     }
     return render(request, 'users/profile.html', context)
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect(reverse('main:index'))

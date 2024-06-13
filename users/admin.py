8dc42071d85dbabc49cd6e29a42ad54a1f8da489
smartone <smart1oneone@gmail.com>
2024-06-13 15:11:53 +0300

    added custom validators

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
+from orders.admin import OrderTabularAdmin
 from users.models import User
 from carts.admin import CartTabAdmin
 @admin.register(User)
 class UsersAdmin(admin.ModelAdmin):
    list_display = ["username", 'first_name',"last_name", "email", ]
 
-   inlines = [CartTabAdmin,]
+   inlines = [CartTabAdmin,OrderTabularAdmin]
 
 
 
